5ae645387fbaf46c2db9ed3a6625991bfcfd1ff7
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-04-21 18:33:09 +0100

    A revolution in paper-thin wrappers.

--- src/main/MaxMeanDispersionSolution.java
+++ src/main/MaxMeanDispersionSolution.java
@@ -2,22 +2,41 @@ package main;
 
 import java.util.ArrayList;
 
-public class MaxMeanDispersionSolution extends ArrayList<Boolean>{
-    MaxMeanDispersionSolution(){
+public class MaxMeanDispersionSolution extends ArrayList<Boolean> {
+    MaxMeanDispersionSolution() {
         super();
     }
-    
-    MaxMeanDispersionSolution(ArrayList<Boolean> solution){
+
+    MaxMeanDispersionSolution(ArrayList<Boolean> solution) {
         super(solution);
     }
 
-    public static ArrayList<Integer> nodesInSolution(ArrayList<Boolean> solution){
+    /*
+     * We don't sacrifice modularity/reusability since we can still use with
+     * unwrapped objecs if we want to (right?).
+     * A revolution in paper-thin wrappers!
+     */
+
+    public ArrayList<Integer> nodesInSolution() {
+        return nodesInSolution(this); // Now you're thinking with portals.
+    }
+    
+    public String toString(){
+        return toString(this);
+    }
+
+    public static ArrayList<Integer> nodesInSolution(ArrayList<Boolean> solution) {
         ArrayList<Integer> nodesInSol = new ArrayList<Integer>();
-        
-        for (int i = 0; i < solution.size(); i++){
-            if (solution.get(i) == true) nodesInSol.add(i);
+
+        for (int i = 0; i < solution.size(); i++) {
+            if (solution.get(i) == true)
+                nodesInSol.add(i);
         }
-        
+
         return nodesInSol;
+    }
+    
+    public static String toString(ArrayList<Boolean> solution){
+        return nodesInSolution(solution).toString();
     }
 }
